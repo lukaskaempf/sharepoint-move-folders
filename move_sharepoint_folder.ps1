fa2ec27052a7c652cd74eea4784199f63091054c
Lukas Kämpf <88829760+lukaskaempf@users.noreply.github.com>
2022-02-23 05:59:45 +0100

    Add files via upload

---
+++ move_sharepoint_folder.ps1
@@ -0,0 +1,14 @@
+﻿#Install-Module -Name "PnP.Powershell"
+
+#Config Variables
+$SiteURL = "https://mpwberatungsteam.sharepoint.com/sites/Daten"
+$SourceFolderURL= "Freigegebene Dokumente/Firmendaten/Auftrage 2020" #Site Relative URL from the current site
+$TargetFolderURL = "/sites/Archiv/Freigegebene Dokumente/Auftrage Archiv" #Server Relative URL of the Target Parent Folder
+ 
+#Connect to PnP Online
+Connect-PnPOnline -Url $SiteURL -Interactive
+  
+  
+#move folder between sites in sharepoint online using powershell
+Move-PnPFile -TargetUrl  $TargetFolderURL -SourceUrl $SourceFolderURL -AllowSchemaMismatch    
+
